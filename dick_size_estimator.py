22313006ca662127c1de5344d948105dbcee4a88
al594 <77206301+al594@users.noreply.github.com>
2022-04-12 14:17:31 +0300

    Add files via upload

---
+++ dick_size_estimator.py
@@ -0,0 +1,13 @@
+x = int(input("enter the amount of your dick"))
+if x < 10:
+	print("very small:(")
+elif 10 <= x < 15:
+		print("small")
+elif 15 <= x < 20:
+	print("good")
+elif 20 <= x < 25:
+	print ("big")
+elif 25 <= x <= 30:
+	print("very big penis :()  ого якiй велiкiй")
+elif x > 30:
+	print("Stop lying ;)")
